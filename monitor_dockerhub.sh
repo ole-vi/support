5eb466b02c66cc8472a634ef817fd49963ec7981
fankaiyuan <172881969@qq.com>
2020-09-04 17:33:08 -0700

    add monitor_dockerhub.sh

---
+++ monitor_dockerhub.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# set username and password
+UNAME="kaiyfan"
+if [[ -z "${UPASS}" ]]; then
+   echo "UPASS env variable not found, please add it first."
+   exit 1
+fi
+
+# get token to be able to talk to Docker Hub
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${UNAME}'", "password": "'${UPASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+#echo $TOKEN
+
+# get list of repos for that user account
+#REPO_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${UNAME}/?page_size=10000 | jq -r '.results|.[]|.name')
+REPO_LIST=(alpine nginx nginx-tags webssh webssh-tags novnc novnc-tags turtleblocksjs turtleblocksjs-tags musicblocks musicblocks-tags node)
+
+# build a list of all images & tags
+for i in ${REPO_LIST[@]}
+do
+  # get tags for repo
+  IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/treehouses/${i}/tags/?page_size=6 | jq -r '.results|.[]|.name')
+
+  # build a list of images from tags
+  for j in ${IMAGE_TAGS}
+  do
+    # add each tag to list
+    FULL_IMAGE_LIST="${FULL_IMAGE_LIST} treehouses/${i}:${j}"
+  done
+  FULL_IMAGE_LIST="${FULL_IMAGE_LIST} --------------------------------------"
+done
+
+# output list of all docker images
+echo "--------------------------------------"
+for i in ${FULL_IMAGE_LIST}
+do
+  echo ${i}
+done
