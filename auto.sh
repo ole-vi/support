261786594ec8caf98e3bedf9635b91be7a27cb7c
root <root@travis-job-ef83c70b-34f0-4f83-8dbc-92b70e206410>
2020-10-03 01:28:49 +0000

    update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,10 +5,14 @@ cd $DIR
 ./automation_source.sh
 sync;sync;sync
 
-if [[ ! -s ./log.diff ]]; then
-  treehouses feedback Docker_hub:'```'$(<log.diff)'```'
+if [[ -s ./log.diff ]]; then
+  treehouses feedback Docker_hub:'```'$(<log.diff)' ```'
+else
+  echo "No changes of docker hub repositories since last time"
 fi
 
-if [[ ! -s ./log_source.diff ]]; then
-treehouses feedback Source:'```'$(<log_source.diff)'```'
+if [[ -s ./log_source.diff ]]; then
+  treehouses feedback Source:'```'$(<log_source.diff)' ```'
+else
+  echo "No changes of sources since last time"	
 fi
